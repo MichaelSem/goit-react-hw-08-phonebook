3ac3b260ab2f2f94de8b949af849448f81ede11f
Semeniuk Mykhailo <micanton0709@gmail.com>
2023-09-18 00:43:14 -0400

    Updates

--- src/Page/Register/Register.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-export const Span = styled.span`
-
-color: #1900D5;
-margin-left: 10px;
-cursor: pointer;
-
-`
--- src/components/Title/Tirtle.styled.jsx
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-
-export const H2 = styled.h2`
-  width: 100%;
-  text-align: center;
-  color: #006400;
-  margin: 0 0 -10px 0;
-`;
